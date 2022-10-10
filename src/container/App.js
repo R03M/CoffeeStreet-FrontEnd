3e3e23522e235cd451ef158e86c4142218a92bbe
DaianaOli <luzdaianaolivaresolivares@gmail.com>
2022-10-10 14:29:48 -0300

    Componentes Landing y About creados

--- src/container/App.js
+++ src/container/App.js
@@ -1,12 +1,16 @@
 import Navbar from "../components/navbar/Navbar"
 import Home from "../components/home/Home"
 import duck from "../media/duckJs2.png"
+import Landing from "../components/Landing/Landing";
+import About from "../components/About/About";
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
+      <Landing/>
+      <About/>
       <Navbar/>
       <img src= {duck} alt= "duckJs"/>
       <Home/>
