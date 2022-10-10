4be3bbb65fec3a50130d34473ed3d5b11e0089c3
DarioMarcuzzi <darik95@hotmail.com>
2022-10-10 19:45:27 -0300

    Landing

--- src/container/App.js
+++ src/container/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../components/home/Home.jsx";
 import SignUp from "../components/signUp/SignUp.jsx";
 import LogIn from "../components/logIn/LogIn.jsx";
+import Landing from "../components/Landing/Landing.jsx";
 import './App.css';
 
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <div className="App">
           <Routes>
-            {/* falta la ruta de la pag inicial (Landing Page) */}
+            <Route path="/" element={<Landing/>}/>
             <Route path="/Home" element={<Home/>}/>
             <Route path="/Sign Up" element={<SignUp/>}/>
             <Route path="/Log In" element={<LogIn/>}/>
