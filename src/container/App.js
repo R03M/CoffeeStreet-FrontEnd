6415e73b4e943c9d21d6e69b14c70932b68549a6
DarioMarcuzzi <darik95@hotmail.com>
2022-10-11 14:06:55 -0300

    paginacion lista

--- src/container/App.js
+++ src/container/App.js
@@ -3,21 +3,6 @@ import Home from "../components/home/Home.jsx";
 import SignUp from "../components/signUp/SignUp.jsx";
 import LogIn from "../components/logIn/LogIn.jsx";
 import Landing from "../components/Landing/Landing.jsx";
-<<<<<<< HEAD
-import './App.css';
-
-
-function App() {
-  return (
-    <div className="App">
-          <Routes>
-            <Route path="/" element={<Landing/>}/>
-            <Route path="/Home" element={<Home/>}/>
-            <Route path="/Sign Up" element={<SignUp/>}/>
-            <Route path="/Log In" element={<LogIn/>}/>
-          </Routes>
-        </div>
-=======
 import Admin from "../components/admin/Admin.jsx";
 import Employee from "../components/employee/Employee.jsx";
 import Client from "../components/client/Client.jsx";
@@ -38,7 +23,6 @@ function App() {
 				<Route path="/employee" element={<Employee />} />
 				<Route path="/client" element={<Client />} />
 				<Route path="*" element={<Error />} />
->>>>>>> 560f06447730c21425d720c9ef465e2b5d830f5b
 
 			</Routes>
 		</div>
