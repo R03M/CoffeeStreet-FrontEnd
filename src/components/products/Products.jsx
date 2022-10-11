4d5b8aacc4d4dca6184dab286bb6b903c9664cca
Mr. Blue <miguelrodriguezcastle@gmail.com>
2022-10-11 19:08:17 -0300

    updated products

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -4,32 +4,46 @@ import { getProducts } from "../../redux/action";
 import NavBar from "../navbar/Navbar";
 import CardP from "./card/CardP";
 import NavbarProduc from "./navbarProducts/NavbarProduc";
+import Pagination from "../pagination/Pagination";
 import "./products.css";
-
 
 const Products = () => {
 	const dispatch = useDispatch();
-	const allProducts = useSelector((state) => state.products)
+	const allProducts = useSelector(state => state.products);
 
-
+	const [currentPage, setCurrentPage] = useState(1);
+	const [productsPerPage, setProductsPerPage] = useState(9);
+	const max = Math.ceil(allProducts.length / productsPerPage);
+	const dataEnd = allProducts.slice(
+		(currentPage - 1) * productsPerPage,
+		(currentPage - 1) * productsPerPage + productsPerPage
+	);
 	useEffect(() => {
-		if(allProducts.length === 0){
+		if (allProducts.length === 0) {
 			dispatch(getProducts());
 		}
 	}, [dispatch]);
 
-
 	return (
 		<div className="productsDiv">
-			<NavBar/>
+			<NavBar />
 			<div className="navbarProduc">
 				<NavbarProduc />
 			</div>
+			{allProducts.length ? (
+				<Pagination
+					currentPage={currentPage}
+					setPage={setCurrentPage}
+					max={max}
+				/>
+			) : (
+				""
+			)}
 
 			<div className="cardsProd">
 				{allProducts.length
-					? allProducts.map(e => {
-							return <CardP product={e} />;
+					? dataEnd.map(data => {
+							return <CardP product={data} />;
 					  })
 					: "LOADING...."}
 			</div>
