48342495663428fce6d87ccf1ff9886c7584d1f5
Mr. Blue <miguelrodriguezcastle@gmail.com>
2022-10-13 19:11:29 -0300

    modifications and updates

---
+++ src/components/productsEdit/navbarPE/NavbarPe.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavbarPe = () => {
+	return (
+		<div>
+
+		</div>
+	);
+}
+
+export default NavbarPe;
--- src/components/signUp/SignUp.jsx
+++ src/components/signUp/SignUp.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import NavBar from '../navbar/Navbar.jsx';
+import "./signUp.css"
 
 const SignUp = () => {
     return (
-        <div>
+        <div className="signUpDiv">
             <NavBar/>
-            <h1>Registrarse</h1>
+            <h1 style={{color: "white", fontSize: "3rem"}}>Proximamente... Sign Up</h1>
         </div>
     );
 }
