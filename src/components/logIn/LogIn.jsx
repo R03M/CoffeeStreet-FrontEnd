91fac60fc40bba05e0b36d4a947945324cbaa015
Mr. Blue <miguelrodriguezcastle@gmail.com>
2022-10-15 20:12:35 -0300

    updated navbar

--- src/components/logIn/LogIn.jsx
+++ src/components/logIn/LogIn.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import NavBar from '../navbar/Navbar.jsx';
-import "./logIn.css"
+import React from "react";
+import NavBar from "../navbar/Navbar.jsx";
+import "./logIn.css";
 
 const LogIn = () => {
-    return (
-        <div className="logInDiv">
-            <NavBar/>
-            <h1 style={{color:"white", fontSize: "3rem"}}>Proximamente... Log In</h1>
-        </div>
-    );
-}
+	return (
+		<div className="logInDiv">
+			<NavBar />
+			<div className="logInBody">
+				<h1 style={{ color: "white", fontSize: "3rem" }}>Proximamente... Log In</h1>
+			</div>
+		</div>
+	);
+};
 
 export default LogIn;
