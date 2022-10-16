a0c61693e1f66c3a16ef688deee5cc8ae62552db
Mr. Blue <miguelrodriguezcastle@gmail.com>
2022-10-16 20:38:03 -0300

    updated component landing

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,39 +1,31 @@
-import React from 'react';
-import logo from "../../media/coffeeStreet.png";
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { RiTeamFill } from "react-icons/ri";
+import { SiCoffeescript } from "react-icons/si";
 import "./Landing.css";
 
 const Landing = () => {
-    return (
-        <div className="contenedor-landing">
-            <div className="card"/>
-            <div className="contendor-descripcion">
-                <img className="img" src= {logo} alt="logo" />
-                <h1>La mejor cafetería de la ciudad</h1>
-                <p>Gran variedad en aroma, sabor, cuerpo, color y textura.
-                Preparados y servidos bajo máximas normas de calidad, brindándoles a nuestros clientes un servicio excelso.
-                </p>
-                    <div className='buttons'>
-                        <Link to="/home">
-                            <button class="learn-more">
-                                <span className="circle" aria-hidden="true">
-                                    <span className="icon arrow"></span>
-                                </span>
-                                <span className="button-text">Learn More</span>
-                            </button>
-                        </Link>
-                        <Link to="/about">
-                            <button class="learn-more">
-                                <span className="circle" aria-hidden="true">
-                                    <span className="icon arrow"></span>
-                                </span>
-                                <span className="button-text">About</span>
-                            </button>
-                        </Link>
-                    </div>
-            </div>
-        </div>
-    );
-}
+	return (
+		<div className="landingDiv">
+			<div className="bodyLandingC">
+				<div className="imgLandingC"></div>
+				<div className="parrfLandingC">
+					Prepared and served under the highest quality standards, providing our customers
+					with a service sublime.
+				</div>
+				<div className="btnsLandingsDivs">
+					<Link to={"/about"}>
+						<button className="btnLandingC"><RiTeamFill /></button>
+					</Link>
+					<Link to={"/home"}>
+						<button className="btnLandingC">
+							<SiCoffeescript/>
+						</button>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Landing;
