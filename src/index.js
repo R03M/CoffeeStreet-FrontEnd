251bd903d9b695ace967f509d9a47c5c8826fe2f
DarioMarcuzzi <darik95@hotmail.com>
2022-10-17 10:22:36 -0300

    LoginAuth0

--- src/index.js
+++ src/index.js
@@ -5,13 +5,23 @@ import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import "./index.css";
 import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider} from '@auth0/auth0-react'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 root.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <App />
+        <Auth0Provider 
+            domain={domain}
+            clientId={clientId} 
+            redirectUri={window.location.origin}/>
+          <App />
+        <Auth0Provider/>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>
