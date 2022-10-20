d270b00a200f38f6269dd46590f5487058504aa3
Mr. Blue <miguelrodriguezcastle@gmail.com>
2022-10-20 13:39:30 -0300

    fix formNewProduct

--- src/components/productsEdit/formNewProducts/schema/productSchema.js
+++ src/components/productsEdit/formNewProducts/schema/productSchema.js
@@ -1,3 +1,4 @@
+import { yupToFormErrors } from "formik";
 import * as Yup from "yup";
 import { ACIDITY } from "../../../../models/acidity.enum";
 import { BITTERNESS } from "../../../../models/bitterness.enum";
@@ -31,8 +32,10 @@ export const productSchema = Yup.object().shape({
 	category: Yup.string()
 		.oneOf(
 			[
-				CATEGORIES.COFFEE,
-				CATEGORIES.TEA,
+				CATEGORIES.COFFEE_READY_TO_DRINK,
+				CATEGORIES.COFFEE_TO_PREPARED,
+				CATEGORIES.TEA_READY_TO_DRINK,
+				CATEGORIES.TEA_TO_PREPARED,
 				CATEGORIES.SWEET_BACKERY,
 				CATEGORIES.SALTY_BACKERY,
 				CATEGORIES.OTHER
@@ -41,156 +44,230 @@ export const productSchema = Yup.object().shape({
 		)
 		.required("Category is required"),
 	//? //////////////////////////////////////////////////////////////////////////////////
-	isPrepared: Yup.string()
-		.oneOf(["false", "true"], "You must a select a Ready to eat or not")
-		.required("Ready to eat? is required"),
-	//? //////////////////////////////////////////////////////////////////////////////////
 	stock: Yup.string()
 		.oneOf(["false", "true"], "You must select whether or not there is stock")
 		.required("Stock is required"),
 	//? //////////////////////////////////////////////////////////////////////////////////
-	lactose: Yup.string().when("category", {
-		is: CATEGORIES.COFFEE,
-		then: Yup.string()
-			.oneOf(["false", "true"], "You must select if it has Lactose")
-			.required("Lactose is required if select coffee"),
-		otherwise: Yup.string().notRequired()
-	}),
-
-
+	lactose: Yup.string()
+		.when("category", {
+			is: CATEGORIES.COFFEE_READY_TO_DRINK,
+			then: Yup.string()
+				.oneOf(["false", "true"], "You must select if it has Lactose")
+				.required("Lactose is required if select coffee"),
+			otherwise: Yup.string().notRequired()
+		})
+		.when("category", {
+			is: CATEGORIES.SALTY_BACKERY,
+			then: Yup.string()
+				.oneOf(["false", "true"], "You must select if it has Lactose")
+				.required("Lactose is required if select Salty backery"),
+			otherwise: Yup.string().notRequired()
+		})
+		.when("category", {
+			is: CATEGORIES.SWEET_BACKERY,
+			then: Yup.string()
+				.oneOf(["false", "true"], "You must select if it has Lactose")
+				.required("Lactose is required if select Sweet backery"),
+			otherwise: Yup.string().notRequired()
+		})
+		.when("category", {
+			is: CATEGORIES.TEA_READY_TO_DRINK,
+			then: Yup.string()
+				.oneOf(["false", "true"], "You must select if it has Lactose")
+				.required("Lactose is required if select Tea ready to drink"),
+			otherwise: Yup.string().notRequired()
+		})
+		.when("category", {
+			is: CATEGORIES.OTHER,
+			then: Yup.string()
+				.oneOf(["false", "true"], "You must select if it has Lactose")
+				.required("Lactose is required if select Other"),
+			otherwise: Yup.string().notRequired()
+		}),
+	//? //////////////////////////////////////////////////////////////////////////////////
+	alcohol: Yup.string()
+		.when("category", {
+			is: CATEGORIES.COFFEE_READY_TO_DRINK,
+			then: Yup.string()
+				.oneOf(["false", "true"], "You must select if it has alcohol")
+				.required("Alcohol is required if select coffee"),
+			otherwise: Yup.string().notRequired()
+		})
+		.when("category", {
+			is: CATEGORIES.SALTY_BACKERY,
+			then: Yup.string()
+				.oneOf(["false", "true"], "You must select if it has Lactose")
+				.required("Lactose is required if select Salty backery"),
+			otherwise: Yup.string().notRequired()
+		})
+		.when("category", {
+			is: CATEGORIES.SWEET_BACKERY,
+			then: Yup.string()
+				.oneOf(["false", "true"], "You must select if it has Lactose")
+				.required("Lactose is required if select Sweet backery"),
+			otherwise: Yup.string().notRequired()
+		})
+		.when("category", {
+			is: CATEGORIES.TEA_READY_TO_DRINK,
+			then: Yup.string()
+				.oneOf(["false", "true"], "You must select if it has Lactose")
+				.required("Lactose is required if select Tea ready to drink"),
+			otherwise: Yup.string().notRequired()
+		})
+		.when("category", {
+			is: CATEGORIES.OTHER,
+			then: Yup.string()
+				.oneOf(["false", "true"], "You must select if it has Lactose")
+				.required("Lactose is required if select Other"),
+			otherwise: Yup.string().notRequired()
+		}),
 	// //? //////////////////////////////////////////////////////////////////////////////////
-	alcohol: Yup.string().when("category", {
-		is: CATEGORIES.COFFEE,
-		then: Yup.string()
-			.oneOf(["false", "true"], "You must select if it has alcohol")
-			.required("Alcohol is required if select coffee"),
-		otherwise: Yup.string().notRequired()
-	}),
-	// //? //////////////////////////////////////////////////////////////////////////////////
-	gluten: Yup.string().when("category", {
-		is: CATEGORIES.COFFEE ,
-		then: Yup.string()
-			.oneOf(["false", "true"], "You must select if it has gluten")
-			.required("Gluten is required if select coffee"),
-		otherwise: Yup.string().notRequired()
-	}),
+	gluten: Yup.string()
+		.when("category", {
+			is: CATEGORIES.COFFEE_READY_TO_DRINK,
+			then: Yup.string()
+				.oneOf(["false", "true"], "You must select if it has gluten")
+				.required("Gluten is required if select coffee"),
+			otherwise: Yup.string().notRequired()
+		})
+		.when("category", {
+			is: CATEGORIES.SALTY_BACKERY,
+			then: Yup.string()
+				.oneOf(["false", "true"], "You must select if it has Lactose")
+				.required("Lactose is required if select Salty backery"),
+			otherwise: Yup.string().notRequired()
+		})
+		.when("category", {
+			is: CATEGORIES.SWEET_BACKERY,
+			then: Yup.string()
+				.oneOf(["false", "true"], "You must select if it has Lactose")
+				.required("Lactose is required if select Sweet backery"),
+			otherwise: Yup.string().notRequired()
+		})
+		.when("category", {
+			is: CATEGORIES.OTHER,
+			then: Yup.string()
+				.oneOf(["false", "true"], "You must select if it has Lactose")
+				.required("Lactose is required if select Other"),
+			otherwise: Yup.string().notRequired()
+		}),
 	// //? //////////////////////////////////////////////////////////////////////////////////
 	// // ingredients: Yup.array().min(1, "At least one ingredient is required").nullable(),
 	// //? //////////////////////////////////////////////////////////////////////////////////
-	// originCountry: Yup.string().when("category", "isPrepared", {
-	// 	is: "category" === CATEGORIES.COFFEE && "isPrepared" === "false",
-	// 	then: Yup.string()
-	// 		.min(4, "The country name is too short")
-	// 		.max(25, "The name of the country is too long")
-	// 		.required("Country of origin is required if coffee is selected to be brewed"),
-	// 	otherwise: Yup.string().notRequired()
-	// }),
-	// //? //////////////////////////////////////////////////////////////////////////////////
-	// cream: Yup.string().when("category", "isPrepared", {
-	// 	is: "category" === CATEGORIES.COFFEE && "isPrepared" === "true",
-	// 	then: Yup.string()
-	// 		.oneOf(["false", "true"], "You must select if it has cream.")
-	// 		.required("Cream is required if ready-to-drink coffee is selected"),
-	// 	otherwise: Yup.string().notRequired()
-	// }),
-	// //? //////////////////////////////////////////////////////////////////////////////////
-	// texture: Yup.string().when("category", "isPrepared", {
-	// 	is: "category" === CATEGORIES.COFFEE && "isPrepared" === "true",
-	// 	then: Yup.string()
-	// 		.oneOf(
-	// 			[
-	// 				TEXTURES.COARSE,
-	// 				TEXTURES.DESERT_DUNES,
-	// 				TEXTURES.FINE,
-	// 				TEXTURES.MEDIUM,
-	// 				TEXTURES.VERY_FINE
-	// 			],
-	// 			"You must a select a texture"
-	// 		)
-	// 		.required("Texture is required if ready-to-drink coffee is selected"),
-	// 	otherwise: Yup.string().notRequired()
-	// }),
+	originCountry: Yup.string().when("category", {
+		is: CATEGORIES.COFFEE_TO_PREPARED,
+		then: Yup.string()
+			.min(4, "The country name is too short")
+			.max(25, "The name of the country is too long")
+			.required("Country of origin is required if coffee is selected to be brewed"),
+		otherwise: Yup.string().notRequired()
+	}),
+	//? //////////////////////////////////////////////////////////////////////////////////
+	cream: Yup.string().when("category", {
+		is: CATEGORIES.COFFEE_READY_TO_DRINK,
+		then: Yup.string()
+			.oneOf(["false", "true"], "You must select if it has cream.")
+			.required("Cream is required if ready-to-drink coffee is selected"),
+		otherwise: Yup.string().notRequired()
+	}),
+	//? //////////////////////////////////////////////////////////////////////////////////
+	texture: Yup.string().when("category", {
+		is: CATEGORIES.COFFEE_READY_TO_DRINK,
+		then: Yup.string()
+			.oneOf(
+				[
+					TEXTURES.COARSE,
+					TEXTURES.DESERT_DUNES,
+					TEXTURES.FINE,
+					TEXTURES.MEDIUM,
+					TEXTURES.VERY_FINE
+				],
+				"You must a select a texture"
+			)
+			.required("Texture is required if ready-to-drink coffee is selected"),
+		otherwise: Yup.string().notRequired()
+	}),
 
-	// //? //////////////////////////////////////////////////////////////////////////////////
-	// body: Yup.string().when("category", "isPrepared", {
-	// 	is: "category" === CATEGORIES.COFFEE && "isPrepared" === "true",
-	// 	then: Yup.string()
-	// 	.oneOf(
-	// 		[BODY.LIGHT, BODY.MEDIUM, BODY.PERCEIVABLE, BODY.SIRUPY, BODY.THICK],
-	// 		"You must a select a body"
-	// 	)
-	// 		.required("Body is required if ready-to-drink coffee is selected"),
-	// 	otherwise: Yup.string().notRequired()
-	// }),
-	// //? //////////////////////////////////////////////////////////////////////////////////
-	// acidity: Yup.string().when("category", "isPrepared", {
-	// 	is: "category" === CATEGORIES.COFFEE && "isPrepared" === "true",
-	// 	then: Yup.string()
-	// 	.oneOf(
-	// 		[
-	// 			ACIDITY.FRESH,
-	// 			ACIDITY.HIGH,
-	// 			ACIDITY.LIGHT,
-	// 			ACIDITY.NOT_FOUND,
-	// 			ACIDITY.PERCEIVABLE
-	// 		],
-	// 		"You must a select a acidity"
-	// 	)
-	// 		.required("Acidity is required if ready-to-drink coffee is selected"),
-	// 	otherwise: Yup.string().notRequired()
-	// }),
-	// //? //////////////////////////////////////////////////////////////////////////////////
-	// bitterness: Yup.string().when("category", "isPrepared", {
-	// 	is: "category" === CATEGORIES.COFFEE && "isPrepared" === "true",
-	// 	then: Yup.string()
-	// 	.oneOf(
-	// 		[
-	// 			BITTERNESS.HIGH,
-	// 			BITTERNESS.LIGHT,
-	// 			BITTERNESS.PERCEIVABLE,
-	// 			BITTERNESS.MEDIUM,
-	// 			BITTERNESS.VERY_HIGH
-	// 		],
-	// 		"You must a select a bitterness"
-	// 	)
-	// 		.required("Body is required if ready-to-drink coffee is selected"),
-	// 	otherwise: Yup.string().notRequired()
-	// }),
-	// //? //////////////////////////////////////////////////////////////////////////////////
-	// roast: Yup.string().when("category", "isPrepared", {
-	// 	is: "category" === CATEGORIES.COFFEE && "isPrepared" === "true",
-	// 	then: Yup.string()
-	// 	.oneOf(
-	// 		[
-	// 			ROAST.CINNAMON,
-	// 			ROAST.CITY,
-	// 			ROAST.DARK,
-	// 			ROAST.FRENCH,
-	// 			ROAST.FULL_CITY,
-	// 			ROAST.ITALIAN,
-	// 			ROAST.LIGHT
-	// 		],
-	// 		"You must a select a roast"
-	// 	)
-	// 		.required("Body is required if ready-to-drink coffee is selected"),
-	// 	otherwise: Yup.string().notRequired()
-	// }),
-	// //? //////////////////////////////////////////////////////////////////////////////////
-	// color: Yup.string().when("category", "isPrepared", {
-	// 	is: "category" === CATEGORIES.COFFEE && "isPrepared" === "true",
-	// 	then: Yup.string()
-	// 	.oneOf(
-	// 		[
-	// 			COLOR.AMBER,
-	// 			COLOR.DARK,
-	// 			COLOR.DARK_BROWN,
-	// 			COLOR.HAZELNUT,
-	// 			COLOR.LIGHT_BROWN,
-	// 			COLOR.YELLOW
-	// 		],
-	// 		"You must a select a color"
-	// 	)
-	// 		.required("Body is required if ready-to-drink coffee is selected"),
-	// 	otherwise: Yup.string().notRequired()
-	// }),
+	//? //////////////////////////////////////////////////////////////////////////////////
+	body: Yup.string().when("category", {
+		is: CATEGORIES.COFFEE_READY_TO_DRINK,
+		then: Yup.string()
+			.oneOf(
+				[BODY.LIGHT, BODY.MEDIUM, BODY.PERCEIVABLE, BODY.SIRUPY, BODY.THICK],
+				"You must a select a body"
+			)
+			.required("Body is required if ready-to-drink coffee is selected"),
+		otherwise: Yup.string().notRequired()
+	}),
+	//? //////////////////////////////////////////////////////////////////////////////////
+	acidity: Yup.string().when("category", {
+		is: CATEGORIES.COFFEE_READY_TO_DRINK,
+		then: Yup.string()
+			.oneOf(
+				[
+					ACIDITY.FRESH,
+					ACIDITY.HIGH,
+					ACIDITY.LIGHT,
+					ACIDITY.NOT_FOUND,
+					ACIDITY.PERCEIVABLE
+				],
+				"You must a select a acidity"
+			)
+			.required("Acidity is required if ready-to-drink coffee is selected"),
+		otherwise: Yup.string().notRequired()
+	}),
+	//? //////////////////////////////////////////////////////////////////////////////////
+	bitterness: Yup.string().when("category", {
+		is: CATEGORIES.COFFEE_READY_TO_DRINK,
+		then: Yup.string()
+			.oneOf(
+				[
+					BITTERNESS.HIGH,
+					BITTERNESS.LIGHT,
+					BITTERNESS.PERCEIVABLE,
+					BITTERNESS.MEDIUM,
+					BITTERNESS.VERY_HIGH
+				],
+				"You must a select a bitterness"
+			)
+			.required("Body is required if ready-to-drink coffee is selected"),
+		otherwise: Yup.string().notRequired()
+	}),
+	//? //////////////////////////////////////////////////////////////////////////////////
+	roast: Yup.string().when("category", {
+		is: CATEGORIES.COFFEE_READY_TO_DRINK,
+		then: Yup.string()
+			.oneOf(
+				[
+					ROAST.CINNAMON,
+					ROAST.CITY,
+					ROAST.DARK,
+					ROAST.FRENCH,
+					ROAST.FULL_CITY,
+					ROAST.ITALIAN,
+					ROAST.LIGHT
+				],
+				"You must a select a roast"
+			)
+			.required("Body is required if ready-to-drink coffee is selected"),
+		otherwise: Yup.string().notRequired()
+	}),
+	//? //////////////////////////////////////////////////////////////////////////////////
+	color: Yup.string().when("category", {
+		is: CATEGORIES.COFFEE_READY_TO_DRINK,
+		then: Yup.string()
+			.oneOf(
+				[
+					COLOR.AMBER,
+					COLOR.DARK,
+					COLOR.DARK_BROWN,
+					COLOR.HAZELNUT,
+					COLOR.LIGHT_BROWN,
+					COLOR.YELLOW
+				],
+				"You must a select a color"
+			)
+			.required("Body is required if ready-to-drink coffee is selected"),
+		otherwise: Yup.string().notRequired()
+	})
 });
