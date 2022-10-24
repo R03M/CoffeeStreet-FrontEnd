9eca5050e72b66fad3c0853e2a05627163c108e2
Mr. Blue <miguelrodriguezcastle@gmail.com>
2022-10-24 13:16:25 -0300

    updated

--- src/components/logIn/loginButtonAuth0/LoginAuth0.jsx
+++ src/components/logIn/loginButtonAuth0/LoginAuth0.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import "./logInAuth0.css"
 
 const LoginAuth0 = () => {
   const { loginWithRedirect } = useAuth0();
   return (
     <div>
-      <button onClick={() => loginWithRedirect()}>Continue with Google</button>
+      <button className="btnContinueGLoginC" onClick={() => loginWithRedirect()}>Continue with Google</button>
     </div>
   );
 };
