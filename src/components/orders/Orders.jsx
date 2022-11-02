5da7cf3e37d3bd4344b6fd9209987bfcfe37bf79
Mr. Blue <miguelrodriguezcastle@gmail.com>
2022-11-02 16:06:18 -0300

    update/Orders:Admin

--- src/components/orders/Orders.jsx
+++ src/components/orders/Orders.jsx
@@ -1,83 +1,71 @@
-import React, { useEffect } from 'react';
-import { getAllOrders,changeStatusOrder } from "../../redux/action";
+import React, { useEffect } from "react";
+import { getAllOrders, changeStatusOrder } from "../../redux/action";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from 'react-router-dom';
-import{GrFormView} from "react-icons/gr";
+import { Link } from "react-router-dom";
+import { MdPreview } from "react-icons/md";
+import swal from "sweetalert";
 import "./orders.css";
-import swal from 'sweetalert';
 
 const Orders = () => {
-	const orders = useSelector((state) => state.ordenes);
-	const product= orders.map((order)=>order.order_product);
+	const orders = useSelector(state => state.ordenes);
+	const product = orders.map(order => order.order_product);
 	const dispatch = useDispatch();
 	// console.log(orders);
 	useEffect(() => {
 		dispatch(getAllOrders());
 	}, [dispatch]);
 
-
-	const handleChangesStatus = (id) => {
+	const handleChangesStatus = id => {
 		if (id) {
-			dispatch(changeStatusOrder(id, {statusDelivery:"complete"}));
+			dispatch(changeStatusOrder(id, { statusDelivery: "complete" }));
 			swal("Good job!", "You change status!", "success");
 			setTimeout(() => {
-			dispatch(getAllOrders());
+				dispatch(getAllOrders());
 			}, 500);
 		} else {
 			swal("Error!", "You not change status!", "error");
 		}
 	};
 
-
 	return (
 		<div className="container">
-			<div className="row">
-				<div className="col-12">
-					<h1>Orders</h1>
-				</div>
-			</div>
-			<div className="row">
-				<div className="col-12">
-					<table className="table table-striped">
-						<thead>
-							<tr>
-								<th scope="col">Id Order</th>
-								<th scope="col">Date</th>
-								<th scope="col">Id User</th>
-								<th scope="col">Total</th>
-								<th scope="col">Status</th>
-								<th scope="col">Actions</th>
-								<th scope="col">Details</th>
-
-							</tr>
-						</thead>
-						<tbody>
-							{orders.map((order) => (
-								<tr key={order.id}>
-									<td>{order.id}</td>
-									<td>{order.date}</td>
-									<td>{order.idUser}</td>
-									<td>$ {order.total}</td>
-									<td>{order.statusDelivery}</td>
-									<td>
-										<button
-											className="btn btn-primary"
-											onClick={() => handleChangesStatus(order.id)}
-										>
-											Change Status
-										</button>
-									</td>
-									<td>
-										<Link to={`/order/${order.id}`}>
-											<GrFormView />
-										</Link>
-									</td>
-								</tr>
-							))}
-						</tbody>
-					</table>
-				</div>
-			</div>
+			<table className="tableOrdersC">
+				<thead className="theadPOrdersC">
+					<tr>
+						<th scope="col">Id Order</th>
+						<th scope="col">Date</th>
+						<th scope="col">Id User</th>
+						<th scope="col">Total</th>
+						<th scope="col">Status</th>
+						<th scope="col">Actions</th>
+						<th scope="col">Details</th>
+					</tr>
+				</thead>
+				<tbody className="tbodyOrdersC">
+					{orders.map(order => (
+						<tr key={order.id}>
+							<td>{order.id}</td>
+							<td>{order.date}</td>
+							<td>{order.idUser}</td>
+							<td>$ {order.total}</td>
+							<td>{order.statusDelivery}</td>
+							<td>
+								<button
+									className="btnChangeStatusOC"
+									onClick={() => handleChangesStatus(order.id)}
+								>
+									Change Status
+								</button>
+							</td>
+							<td>
+								<Link to={`/order/${order.id}`}>
+									<MdPreview className="btnDetailsOC"/>
+								</Link>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</div>
 	);
 };
