a39171c099c18fc6d2603ed2c0c551b150767b6f
Mr. Blue <miguelrodriguezcastle@gmail.com>
2022-11-02 23:09:34 -0300

    update/MyOrders:Client

--- src/components/admin/profits/Profits.jsx
+++ src/components/admin/profits/Profits.jsx
@@ -1,85 +1,82 @@
-import React,{ useEffect, useState } from "react";
-// import TempNd from "../../tempNoDelete/TempNd";
-import "./profits.css";
-import GraphicsLine from "./graphics/graphicsLine/GraphicsLine.jsx"
+import React, { useEffect, useState } from "react";
+import GraphicsLine from "./graphics/graphicsLine/GraphicsLine.jsx";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { getAllOrders} from "../../../redux/action";
-
+import { getAllOrders } from "../../../redux/action";
+import "./profits.css";
 
 const Profits = () => {
-	
-	const [sumaTotal, setSumaTotal] = useState(0);
+	const dispatch = useDispatch();
+	const ordenes = useSelector(state => state.ordenes);
 	const [datosGraphicsLine, setDatosGraphicsLine] = useState([]);
 	const [datosGraphicsLine2, setDatosGraphicsLine2] = useState([]);
-	const ordenes = useSelector((state) => state.ordenes);
-	const dispatch = useDispatch();
+	const [sumaTotal, setSumaTotal] = useState(0);
+
 	const [dataGraphics, setDataGraphics] = useState({
-		labels:"",
-		datasets:[{
-			label:"",
-			data:"",
-			backgroundColor:"",
-		}]
-	})
-	
-	
-	
-useEffect(() => {
-	dispatch(getAllOrders());
+		labels: "",
+		datasets: [
+			{
+				label: "",
+				data: "",
+				backgroundColor: ""
+			}
+		]
+	});
 
-}, [dispatch]);
-
-	
 	useEffect(() => {
-	
-		setSumaTotal(ordenes.reduce((a, b) => a + b.total, 0));
-		setDatosGraphicsLine(ordenes.map((e)=>{return {date: e.date.slice(0,10), cantidad: e.total }}))
+		dispatch(getAllOrders());
+	}, [dispatch]);
 
+	useEffect(() => {
+		setSumaTotal(ordenes.reduce((a, b) => a + b.total, 0));
+		setDatosGraphicsLine(
+			ordenes.map(e => {
+				return { date: e.date.slice(0, 10), cantidad: e.total };
+			})
+		);
 	}, [ordenes]);
 
 	useEffect(() => {
-
-		setDatosGraphicsLine2(datosGraphicsLine.reduce((a, b) => {
-			if (a.some((e) => e.date === b.date)) {
-				a.find((e) => e.date === b.date).cantidad += b.cantidad;
-			} else {
-				a.push(b);
-			}
-			return a;
-		}, []));
-
+		setDatosGraphicsLine2(
+			datosGraphicsLine.reduce((a, b) => {
+				if (a.some(e => e.date === b.date)) {
+					a.find(e => e.date === b.date).cantidad += b.cantidad;
+				} else {
+					a.push(b);
+				}
+				return a;
+			}, [])
+		);
 	}, [datosGraphicsLine]);
 
-
-	
-	useEffect (()=>{
+	useEffect(() => {
 		setDataGraphics({
-			labels:[...datosGraphicsLine2.map((e)=>{return e.date})],
-			datasets:[{                                         
-				label:"sales in the month",
-				data : [...datosGraphicsLine2.map((e)=>{return e.cantidad})],
-				backgroundColor:["red", "blue", "green", "yellow", "pink", "orange"],
-			}]
-		})
-	}
-	,[datosGraphicsLine2])
-	
-	
+			labels: [
+				...datosGraphicsLine2.map(e => {
+					return e.date;
+				})
+			],
+			datasets: [
+				{
+					label: "sales in the month",
+					data: [
+						...datosGraphicsLine2.map(e => {
+							return e.cantidad;
+						})
+					],
+					backgroundColor: ["red", "blue", "green", "yellow", "pink", "orange"]
+				}
+			]
+		});
+	}, [datosGraphicsLine2]);
+
 	return (
 		<div className="profitsDivC">
-			{/* <TempNd /> */}
-			<div className="top-sellers">
-				<div className="top-sellers-title-page"><h1> Sales magnament report</h1></div>
-				<div className="top-sellers-div1"><></></div>
-				<div className="top-sellers-div2"><></></div>
-				<div className="top-sellers-div3"><></></div>
-			</div>
-			<div >
-				<GraphicsLine whith={600} charData={dataGraphics} />
+			<div>
+				<GraphicsLine whith={800} charData={dataGraphics}/>
 			</div>
 			<div>
-				  <h1>Total profits :  { Math.round(sumaTotal) } $ </h1>
+				<h1>Total profits : {Math.round(sumaTotal)} $ </h1>
 			</div>
 		</div>
 	);
--- src/components/client/myOrders/MyOrders.jsx
+++ src/components/client/myOrders/MyOrders.jsx
@@ -1,194 +1,201 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./myOrders.css";
-import {getOrdersByUser,getReviewByUser,deleteReviews} from "../../../redux/action";
+import { getOrdersByUser, getReviewByUser, deleteReviews } from "../../../redux/action";
 import Reviews from "./reviews/reviews";
 import ChangeDescriptionR from "./changeDescriptionR";
 import ChangeRating from "./changeRating";
 import swal from "sweetalert";
-
+import RowsMyOrder from "./rowsMyOrder/RowsMyOrder";
 
 const MyOrders = () => {
 	const dispatch = useDispatch();
-	const ordenes = useSelector((state) => state.ordenes);
-	const ordenesFilter = useSelector((state) => state.ordenesFilter);
-	const filter = useSelector((state) => state.filterUserOrden);
-	const user = useSelector((state) => state.user);
-	const reviewCreated = useSelector((state) => state.reviewsUser);
+	const ordenes = useSelector(state => state.ordenes);
+	const ordenesFilter = useSelector(state => state.ordenesFilter);
+	const filter = useSelector(state => state.filterUserOrden);
+	const user = useSelector(state => state.user);
+	const reviewCreated = useSelector(state => state.reviewsUser);
 	const [review, setReview] = useState(false);
 	const [changeDescription, setChangeDescription] = useState(false);
 	const [changeRating, setChangeRating] = useState(false);
 
-
 	useEffect(() => {
-		if(!filter) {
-			dispatch(getOrdersByUser(user.user.id))
+		if (!filter) {
+			dispatch(getOrdersByUser(user.user.id));
 		}
-	}, [dispatch,filter,user.user.id]);
+	}, [dispatch, filter, user.user.id]);
 
 	useEffect(() => {
-		dispatch(getReviewByUser(user.user.id))
-	}, [dispatch, user.user.id, ])
+		dispatch(getReviewByUser(user.user.id));
+	}, [dispatch, user.user.id]);
 
-	const filterOrdenClient =(e) =>{
-		dispatch({type: "FILTER_ORDEN_CLIENT", payload: e.target.value})
-
-	}
+	const filterOrdenClient = e => {
+		dispatch({ type: "FILTER_ORDEN_CLIENT", payload: e.target.value });
+	};
 
 	const handleReview = () => {
-		if(review === false){
-			setReview(true)
-		}else{
-			setReview(false)
+		if (review === false) {
+			setReview(true);
+		} else {
+			setReview(false);
 		}
-	}
+	};
 
 	const changeDescriptions = () => {
-		if(changeDescription === false){
-			setChangeDescription(true)
-		}else{
-			setChangeDescription(false)
+		if (changeDescription === false) {
+			setChangeDescription(true);
+		} else {
+			setChangeDescription(false);
 		}
-	}
+	};
 
 	const changeRatings = () => {
-		if(changeRating === false){
-			setChangeRating(true)
-		}else{
-			setChangeRating(false)
+		if (changeRating === false) {
+			setChangeRating(true);
+		} else {
+			setChangeRating(false);
 		}
-	}
+	};
 
-	const handleDelete = (id) => {
-		dispatch(deleteReviews( reviewCreated[0].id))
+	const handleDelete = id => {
+		dispatch(deleteReviews(reviewCreated[0].id));
 		swal({
 			title: "Review deleted",
 			icon: "success",
-			button: "Ok",
-		})
-	}
+			button: "Ok"
+		});
+	};
 
-// console.log("ordenes", ordenes)
-// console.log("rc", reviewCreated)
+	// console.log("ordenes", ordenes)
+	// console.log("rc", reviewCreated)
 
 	return (
-		<div className='contenedor-principal'>
-			<div	className='filtros-orden'>
-			  <div className="titleFilter">
-					<label>Filter by status</label>
-				</div>
-				<div className='buttons-orden'>
-					<button  onClick={filterOrdenClient} value="All">All</button>
-					<button  onClick={filterOrdenClient} value="complete">Completed</button>
-					<button  onClick={filterOrdenClient} value="pending">Pending</button>
-				</div>
-				<div className="contenedor-review">
-				    {ordenes.length === 0 ? (
-							<div>
-								<h1>debe hacer una compra para hacer una review</h1>
-							</div>
-						) :  reviewCreated.length === 0 ? (
-							<div className="contenedor-review-inputs">
-								<button onClick={handleReview}>Make a review</button>
-								{ review ? <Reviews /> : null}
-							</div>
-						) : (
-							<div className="contenedor-user-review">
-								<div className="contenedor-data-review">
-									{changeDescription ? <ChangeDescriptionR setChangeDescription={setChangeDescription}  /> : <p>{reviewCreated[0].description}</p>}
-									{/* <p>{reviewCreated[0].description}</p> */}
-									{changeRating ? <ChangeRating setChangeRating ={setChangeRating} /> : <p>{reviewCreated[0].rating}</p>}
-									{/* <p>{reviewCreated[0].rating}</p> */}
-									<button onClick={handleDelete}>Delete Review</button>
-								</div>
+		<div className="myOrdersDivC">
+			<div className="reviewOrdersDivC">
+				{ordenes.length === 0 ? (
+					<div>
+						<h1>You must make a purchase to review</h1>
+					</div>
+				) : reviewCreated.length === 0 ? (
+					<div className="contenedor-review-inputs">
+						<button onClick={handleReview}>Make a review</button>
+						{review ? <Reviews /> : null}
+					</div>
+				) : (
+					<div className="contenedor-user-review">
+						<div className="contenedor-data-review">
+							{changeDescription ? (
+								<ChangeDescriptionR setChangeDescription={setChangeDescription} />
+							) : (
+								<p>{reviewCreated[0].description}</p>
+							)}
+							{/* <p>{reviewCreated[0].description}</p> */}
+							{changeRating ? (
+								<ChangeRating setChangeRating={setChangeRating} />
+							) : (
+								<p>{reviewCreated[0].rating}</p>
+							)}
+							{/* <p>{reviewCreated[0].rating}</p> */}
+							<button onClick={handleDelete}>Delete Review</button>
+						</div>
 
-								<div className="btn-change-review">
-									<button onClick={changeDescriptions}>Change Description</button>
-									<button onClick={changeRatings}>Change Rating</button>
-								</div>
-
-							</div>
-						)}
-				</div>
+						<div className="btn-change-review">
+							<button onClick={changeDescriptions}>Change Description</button>
+							<button onClick={changeRatings}>Change Rating</button>
+						</div>
+					</div>
+				)}
 			</div>
-			<div className='contenedor-ordenes'>
-			{filter ? (
-				ordenesFilter.map(orden => {
-					return (
-						<div className='Contenedor-Orden'>
-						  <div className={orden.statusDelivery === "pending"? "status-orden-pending" : orden.statusDelivery === "complete" ? "status-orden-completed" : "status-orden-cancelado" }>
-									<h2>{orden.statusDelivery}</h2>
-							</div>
-							<div className='cuerpo-Orden'>
-							<thead>
-							<tr className="title-orden">
-								<th className="name-product-orden" >Name</th>
-								<th  className="quantity-product-orden">quantity</th>
-								<th className="price-product-orden" >price</th>
-							</tr>
-						</thead>
-						 <tbody>
-								{orden.order_product.map(item => {
-									return (
-										<div className='item-orden'>
-											<h4>{item.product.name}</h4>
-											<h4> {item.quantity}</h4>
-											<h2 className='price-item'>{item.total / item.quantity}$</h2>
+
+			<div className="bodyMyOrdersC">
+				<div className="navOrdersC">
+					<p style={{fontWeight: "700"}}>Filter by status</p>
+					<button className="btnNavbarMyOrdersC" onClick={filterOrdenClient} value="All">
+						All
+					</button>
+					<button className="btnNavbarMyOrdersC" onClick={filterOrdenClient} value="complete">
+						Completed
+					</button>
+					<button className="btnNavbarMyOrdersC" onClick={filterOrdenClient} value="pending">
+						Pending
+					</button>
+				</div>
+				<div className="cardsMyOrdersC">
+					{filter
+						? ordenesFilter.map(orden => {
+								return (
+									<div className="tableCardMyOrdersC">
+										<div className="totalMyOrderTC">
+											<p className="statusMyOrderC">Status:</p>
+											<p
+												className={
+													orden.statusDelivery === "complete"
+														? "completeStyleMyOrdC"
+														: "pendingStyleMyOrdC"
+												}
+											>
+												{orden.statusDelivery}
+											</p>
 										</div>
-									)
-								})}
-							</tbody>
-
-							<div className='total-orden'>
-								<h1>Total: {orden.total}$</h1>
-							</div>
-							</div>
-
-						</div>
-					)
-
-				})
-			) : (
-				ordenes.map(orden => {
-					return (
-						<div className='Contenedor-Orden'>
-						  <div className={orden.statusDelivery === "pending"? "status-orden-pending" : orden.statusDelivery === "complete" ? "status-orden-completed" : "status-orden-cancelado" }>
-									<h2>{orden.statusDelivery}</h2>
-							</div>
-							<div className='cuerpo-Orden'>
-							<thead>
-							<tr className="title-orden">
-								<th className="name-product-orden" >Name</th>
-								<th  className="quantity-product-orden">quantity</th>
-								<th className="price-product-orden" >price</th>
-							</tr>
-						</thead>
-						 <tbody>
-								{orden.order_product.map(item => {
-									return (
-										<div className='item-orden'>
-											<h4>{item.product.name}</h4>
-											<h4> {item.quantity}</h4>
-											<h2 className='price-item'>{item.total / item.quantity}$</h2>
+										<table style={{ width: "100%" }}>
+											<thead className="theadMyOrdersC">
+												<tr>
+													<th>Name</th>
+													<th>Quantity</th>
+													<th>Price</th>
+												</tr>
+											</thead>
+											<tbody>
+												{orden.order_product.map(item => {
+													return <RowsMyOrder item={item} />;
+												})}
+											</tbody>
+										</table>
+										<div className="totalMyOrderTC">
+											<p className="totalMyOrderC">Total:</p>
+											<p className="totalMyOrderC">$ {orden.total}</p>
 										</div>
-									)
-								})}
-							</tbody>
-
-							<div className='total-orden'>
-								<h1>Total: {orden.total}$</h1>
-							</div>
-							</div>
-
-						</div>
-					)
-
-				})
-			)}
-
+									</div>
+								);
+						  })
+						: ordenes.map(orden => {
+								return (
+									<div className="tableCardMyOrdersC">
+										<table style={{ width: "100%" }}>
+											<thead className="theadMyOrdersC">
+												<tr>
+													<th>Name</th>
+													<th>Quantity</th>
+													<th>Price</th>
+												</tr>
+											</thead>
+											<tbody>
+												{orden.order_product.map(item => {
+													return <RowsMyOrder item={item} />;
+												})}
+											</tbody>
+										</table>
+										<div className="totalMyOrderTC">
+											<p className="statusMyOrderC">Status:</p>
+											<p
+												className={
+													orden.statusDelivery === "complete"
+														? "completeStyleMyOrdC"
+														: "pendingStyleMyOrdC"
+												}
+											>
+												{orden.statusDelivery}
+											</p>
+											<p className="totalMyOrderC">Total:</p>
+											<p className="totalMyOrderC">{orden.total}$</p>
+										</div>
+									</div>
+								);
+						  })}
+				</div>
 			</div>
 		</div>
 	);
-}
+};
 
 export default MyOrders;
