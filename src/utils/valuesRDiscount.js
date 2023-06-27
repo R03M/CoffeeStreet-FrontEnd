50d3ad68ef8adff5711eef8ffd6fd43f4558065a
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-06-27 11:34:31 -0300

    Update: simplifies logic

--- src/utils/valuesRDiscount.js
+++ src/utils/valuesRDiscount.js
@@ -1,12 +1,16 @@
+const discounts = {
+	0.1: "off 10%",
+	0.2: "off 20%",
+	0.3: "off 30%",
+	0.4: "off 40%",
+	0.5: "off 50%",
+	0.6: "off 60%",
+	0.7: "off 70%",
+	0.8: "off 80%",
+	0.9: "off 90%",
+	1: "off 100%"
+};
+
 export const valuesRDiscount = value => {
-	if (value === 0.1) return "off 10%";
-	if (value === 0.2) return "off 20%";
-	if (value === 0.3) return "off 30%";
-	if (value === 0.4) return "off 40%";
-	if (value === 0.5) return "off 50%";
-	if (value === 0.6) return "off 60%";
-	if (value === 0.7) return "off 70%";
-	if (value === 0.8) return "off 80%";
-	if (value === 0.9) return "off 90%";
-	if (value === 1) return "off 100%";
+	return discounts[value];
 };
